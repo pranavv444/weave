6de3cea34828256916158b0dc7085880dc5073c9
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-09-21 01:53:24 +0530

    create sign-up page and

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,9 @@
+import { z } from "zod";
+
+
+export const SignupValidation = z.object({
+    name: z.string().min(2,{message:'Too short'}).max(50),
+    username: z.string().min(2,{message:'have a unique username'}).max(50),
+    email: z.string().email(),
+    password: z.string().min(8,{message:'Looks like you want a weak password'}),
+  });
