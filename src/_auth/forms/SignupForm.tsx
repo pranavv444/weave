0bf41186c322a8e9a4025dbcd75cf2080657fd23
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-10-05 05:27:26 +0530

    safe commit

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -2,7 +2,6 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
@@ -13,8 +12,12 @@ import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
+import Loader from "@/components/shared/Loader";
+import { Link } from "react-router-dom";
+import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
+  const isLoading=false;
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
     resolver: zodResolver(SignupValidation),
@@ -27,35 +30,89 @@ const SignupForm = () => {
   });
 
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof SignupValidation>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values);
+  async function onSubmit(values: z.infer<typeof SignupValidation>) {
+    const newUser=await createUserAccount(values);
+
+    console.log(newUser);
+
   }
   return (
     <Form {...form}>
       <div className="sm:w-420 flex-center flex-col">
-        <img src="" alt=""/>
+        <img src="/assets/images/logo.svg" alt="" />
+        <h2 className="h3-bold md:h2-bold pt-5 sm:pt-12">
+          {" "}
+          Create a new account
+        </h2>
+        <p className="text-light-3 small-medium md:base-regualar mt-2">
+          To use Weave please enter your account credentials
+        </p>
+
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 flex flex-col gap-5 w-full mt-4">
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Name</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="username"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Username</FormLabel>
+                <FormControl>
+                  <Input type="text" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>E-mail</FormLabel>
+                <FormControl>
+                  <Input type="email" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input type="password" className="shad-input" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" className="shad-button_primary">
+            {isLoading?(
+              <div className="flex-center gap-2"> <Loader/> Loading...
+              </div>
+            ):"Sign up"}
+          </Button>
+          <p className="text-small-regular text-light-2 text-center mt-2">
+            Already have an account?
+            <Link to="/sign-in" className="text-primary-500 text-small-semibold ml-1">Log In</Link>
+          </p>
+        </form>
       </div>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
     </Form>
   );
 };
---
+++ src/lib/appwrite/api.ts
@@ -0,0 +1,24 @@
+import { ID } from "appwrite";
+import { INewUser } from "@/types";
+import { account } from "./config";
+
+export async function createUserAccount(user:INewUser){
+    try{
+        const newAccount=await account.create(
+            ID.unique(),
+            user.email,
+            user.password,
+            user.name,
+        );
+        if(!newAccount){
+            throw new Error('Account not created');
+        }
+        return newAccount;
+
+    }
+    catch(error){
+        console.log(error)
+        return error;
+    }
+
+}
