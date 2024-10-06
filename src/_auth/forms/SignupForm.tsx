cc5d17c533114d14b23f71a6e1e15cdf34c6ac41
Pranav Soan <pranav.soan.cs21@ggits.net>
2024-10-07 04:16:54 +0530

    safe commit-2

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -12,11 +12,13 @@ import { Button } from "@/components/ui/button";
 import { useForm } from "react-hook-form";
 import { SignupValidation } from "@/lib/validation";
 import { z } from "zod";
+import { useToast } from "@/hooks/use-toast";
 import Loader from "@/components/shared/Loader";
 import { Link } from "react-router-dom";
 import { createUserAccount } from "@/lib/appwrite/api";
 
 const SignupForm = () => {
+  const {toast}=useToast();
   const isLoading=false;
   // 1. Define your form.
   const form = useForm<z.infer<typeof SignupValidation>>({
@@ -32,9 +34,11 @@ const SignupForm = () => {
   // 2. Define a submit handler.
   async function onSubmit(values: z.infer<typeof SignupValidation>) {
     const newUser=await createUserAccount(values);
+    if(!newUser){
+      return toast({title:'Sign up failed. Please try again'})
+    }
 
-    console.log(newUser);
-
+    // const session=await signInAccount();
   }
   return (
     <Form {...form}>
--- src/lib/appwrite/api.ts
+++ src/lib/appwrite/api.ts
@@ -1,24 +1,49 @@
 import { ID } from "appwrite";
 import { INewUser } from "@/types";
-import { account } from "./config";
+import { account, appwriteConfig, avatars, databases } from "./config";
 
-export async function createUserAccount(user:INewUser){
-    try{
-        const newAccount=await account.create(
+export async function createUserAccount(user: INewUser) {
+    try {
+        const newAccount = await account.create(
             ID.unique(),
             user.email,
             user.password,
             user.name,
         );
-        if(!newAccount){
-            throw new Error('Account not created');
-        }
-        return newAccount;
+        if (!newAccount) throw Error;
+        const avatarUrl = new URL(avatars.getInitials(user.name)); // Convert to URL
+        // const avatarUrl=avatars.getInitials(user.name);
+        const newUser = await saveUserToDB({
+            accountId: newAccount.$id,
+            name: newAccount.name,
+            email: newAccount.email,
+            username: user.username,
+            imageUrl: avatarUrl,
+        });
+        return newUser;
 
-    }
-    catch(error){
-        console.log(error)
+    } catch (error) {
+        console.log(error);
         return error;
     }
+}
 
+export async function saveUserToDB(user: {
+    accountId: string;
+    name: string;
+    email: string;
+    imageUrl: URL; 
+    username?: string;
+}) {
+    try {
+        const newUser = await databases.createDocument(
+            appwriteConfig.databaseId,
+            appwriteConfig.userCollectionId,
+            ID.unique(),
+            user,
+        );
+        return newUser; // Added return statement
+    } catch (error) {
+        console.log(error);
+    }
 }
